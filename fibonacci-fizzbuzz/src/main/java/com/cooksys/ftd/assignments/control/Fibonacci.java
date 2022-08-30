95f418417b941ca647191085e9f36934e9d2288d
Emmanuel Azike <100241392+ejjsuper123@users.noreply.github.com>
2022-08-29 23:41:32 -0400

    Add files via upload

---
+++ fibonacci-fizzbuzz/src/main/java/com/cooksys/ftd/assignments/control/Fibonacci.java
@@ -0,0 +1,100 @@
+package com.cooksys.ftd.assignments.control;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * The Fibonacci sequence is simply and recursively defined: the first two elements are `1`, and
+ * every other element is equal to the sum of its two preceding elements. For example:
+ * <p>
+ * [1, 1] =>
+ * [1, 1, 1 + 1]  => [1, 1, 2] =>
+ * [1, 1, 2, 1 + 2] => [1, 1, 2, 3] =>
+ * [1, 1, 2, 3, 2 + 3] => [1, 1, 2, 3, 5] =>
+ * ...etc
+ */
+public class Fibonacci {
+	public static void main(String[] args) {
+		System.out.println(atIndex(10));
+		
+	}
+
+    /**
+     * Calculates the value in the Fibonacci sequence at a given index. For example,
+     * `atIndex(0)` and `atIndex(1)` should return `1`, because the first two elements of the
+     * sequence are both `1`.
+     *
+     * @param i the index of the element to calculate
+     * @return the calculated element
+     * @throws IllegalArgumentException if the given index is less than zero
+     */
+    public static int atIndex(int i) throws IllegalArgumentException {
+    	int first = 1;
+    	int secound = 1;
+    	int addIndex = 0;
+    	
+    	
+        if(i < 0) {
+        	throw new IllegalArgumentException();
+        	}
+    	
+    	if(i == 0 || i == 1) {
+        	return 1;
+        }
+    	
+    	for(int j = 2; j <= i; j++){	
+    		
+    		
+    		addIndex = first+secound;
+    		first = secound;
+    		secound = addIndex;
+    	}
+    	return addIndex;
+    }
+    /**
+     * Calculates a slice of the fibonacci sequence, starting from a given start index (inclusive) and
+     * ending at a given end index (exclusive).
+     *
+     * @param start the starting index of the slice (inclusive)
+     * @param end   the ending index of the slice(exclusive)
+     * @return the calculated slice as an array of int elements
+     * @throws IllegalArgumentException if either the given start or end is negative, or if the
+     *                                  given end is less than the given start
+     */
+    public static int[] slice(int start, int end) throws IllegalArgumentException {
+    	
+    	int arrLength = (end - start);
+    	int[] newArr = new int[arrLength];
+    	
+        
+    	if((start > end)||(end < 0)||(start < 0)){
+    		throw new IllegalArgumentException();
+    	}
+    	for(int i = 0; i < arrLength; i++, start++) {
+    		newArr[i] = atIndex(start);
+    	}
+    	return newArr;
+    }
+
+    /**
+     * Calculates the beginning of the fibonacci sequence, up to a given count.
+     *
+     * @param count the number of elements to calculate
+     * @return the beginning of the fibonacci sequence, up to the given count, as an array of int elements
+     * @throws IllegalArgumentException if the given count is negative
+     */
+    public static int[] fibonacci(int count) throws IllegalArgumentException {
+
+    	if(count < 0) {
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	int[] newArr = new int[count];
+    	
+    	for(int i = 0; i<count; i++) {
+    		newArr[i] = atIndex(i);
+    		
+    	}
+    	return newArr;
+    }
+    
+}
